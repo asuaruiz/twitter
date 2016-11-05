c8703a1a8fe575fcaf64cabe0786ce44f1700691
Andrea <Andrea>
2016-11-04 21:59:25 -0300

    agregadas funciones, pagina funcional pero faltan estilos

--- js/main.js
+++ js/main.js
@@ -1,26 +1,63 @@
-
+""
 //variables
 var lista = document.getElementById("lista"),
 	tarea = document.getElementById("tareaInput"),
-	btnInput = document.getElementById("btnInput");
+	btnInput = document.getElementById("btnInput"),
+	checkBox = document.createElement("input"),
+	trashCan = document.createElement("i");
 
 	//funciones
-var comprobarInput = function(){
-	
-};
 var agregarTarea = function(){
-	var tarea = tareaInput.value,
-		nuevaTarea = document.createElement("li"),
-		enlace = document.createElement("a"),
+		tarea = tareaInput.value;
+		nuevaTarea = document.createElement("li");
+		enlace = document.createElement("span");
 		contenido = document.createTextNode(tarea);
 
 
 	if (tarea === ""){
 		tareaInput.setAttribute("placeholder","agrega una tarea Valida");
-		tareaInput.className = "error";
+		tareaInput.className = "error, form-control";
 		return false;
-
 	}
+	//dar atributos checkbox
+	checkBox.setAttribute("type","checkbox");
+	checkBox.setAttribute("onChange","checkear()")
+	//agregar checkbox
+	nuevaTarea.appendChild(checkBox);
+	//agregamos el contenido al enlace
+	enlace.appendChild(contenido);
+	//establecer un atributo href
+	//enlace.setAttribute("href", "#");
+	//agregamos el enlace (a) a la nueva tarea (li)
+	nuevaTarea.appendChild(enlace);
+	//dar atributos trashCan
+	trashCan.classList.add("fa","fa-minus-square");
+	//agregar trashCan
+	nuevaTarea.appendChild(trashCan);
+	//agregamos nueva tarea a la lista
+	lista.appendChild(nuevaTarea);
+	//limpiamos el textarea despues de agregar tarea
+	tareaInput.value = "";
+
+
+//<i class="fa fa-minus-square" aria-hidden="true"></i>
+
+	
+	for (var i = 0; i< lista.children.length; i++) {
+		lista.children[i].children[2].addEventListener("click", function(){
+			//this.parentNode.removeChild(this);
+			var li = this.parentNode;
+			lista.removeChild(li);
+	});
+}	
+};
+function checkear(){
+	enlace.setAttribute("class","tachado");
+}
+
+var comprobarInput = function(){
+	tareaInput.className = "form-control";
+	tareaInput.setAttribute("placeholder", "agrega tu tarea")
 };
 function eliminarTarea() {
 	alert("eliminar tarea");
@@ -32,6 +69,9 @@ btnInput.addEventListener("click", agregarTarea);
 
 //comprobar input
 tareaInput.addEventListener("click", comprobarInput);
+
+//tachar
+//checkBox.addEventListener("click", checkear);
 
 //borrar elementos lista
 for (var i = 0; i<= lista.children.length -1; i++) {
